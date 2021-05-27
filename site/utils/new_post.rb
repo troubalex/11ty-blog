a36ea33231ea6d0fc3459cefed190722e82924fd
Alexandra Leisse <troubalex@gmail.com>
2021-05-27 20:18:14 +0200

    scripting because I am lazy

---
+++ site/utils/new_post.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/ruby
+
+require 'date'
+
+$file_name = ARGV[0]
+$creation_date = Time.now.strftime("%Y-%m-%d")
+
+Dir.chdir "posts"
+
+File.open("#{$creation_date}-#{$file_name}.md", "w+") do |f|
+  f.write("---
+title:
+url:
+description: ''
+date: #{$creation_date}
+social_image: /img/
+tags:
+  -
+---")
+end
